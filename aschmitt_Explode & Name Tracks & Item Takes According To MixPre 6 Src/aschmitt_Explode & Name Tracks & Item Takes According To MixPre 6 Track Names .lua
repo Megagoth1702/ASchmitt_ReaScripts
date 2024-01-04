f844c86ef406cee9788eb0578d8040abd9c149e8
Megagoth1702 <megagoth@gmx.net>
2024-01-04 17:19:32 +0100

    Changed: moving things around

--- aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Src/aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Track Names .lua
+++ aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Src/aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Track Names .lua
@@ -1,3 +1,16 @@
+-- @description Explode MulChan & Name Tracks & Item Takes According To MixPre 6 Track Names 
+-- @author Andrej Schmitt
+-- @version 1.0
+-- @about
+--   This is an example of a package file. It installs itself as a ReaScript that
+--   does nothing but show "Hello World!" in REAPER's scripting console.
+--
+--   Packages may also include additional files specified using the @provides tag.
+--
+--   This text is the documentation shown when using ReaPack's "About this package"
+--   feature. [Markdown](https://commonmark.org/) *formatting* is supported.
+
+
 -- Check if REAPER is available
 if not reaper then
   error("This script requires REAPER to run.")
