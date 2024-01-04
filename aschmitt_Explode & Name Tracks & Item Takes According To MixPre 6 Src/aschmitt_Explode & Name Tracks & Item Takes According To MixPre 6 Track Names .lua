66e809fad3c78b49800516ed709e890370e14473
Megagoth1702 <megagoth@gmx.net>
2024-01-04 17:24:11 +0100

    afs

--- aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Src/aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Track Names .lua
+++ aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Src/aschmitt_Explode & Name Tracks & Item Takes According To MixPre 6 Track Names .lua
@@ -3,7 +3,7 @@
 -- @version 1.0
 -- @about
 --   This is an example of a package file. It installs itself as a ReaScript that
---   does nothing but show "Hello World!" in REAPER's scripting console.
+--   does nothing but show "Hello World!" in REAPER's scripting console..
 --
 --   Packages may also include additional files specified using the @provides tag.
 --
